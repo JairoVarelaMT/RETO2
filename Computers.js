ac24fafed1878e1ab43e0ca0fc808416fe857d95
JairoVarelamt <jairo.varela.mt@correo.usa.edu.co>
2021-10-10 18:41:21 -0500

    se agregar formulario computer con su tabla, uso de los servicios GET,POST,PUT Y DELETE

---
+++ Computers.js
@@ -0,0 +1,161 @@
+$(document).ready(function(){
+    $("#computers").hide();
+    $("#btnComputers").click(function(){
+        $("#computers").show();
+    })
+    $("#btnGuardar").hide();
+})
+function agregar(){
+    var data={
+        id:$("#id").val(),
+        name:$("#name").val(),
+        brand:$("#brand").val(),
+        model:$("#model").val(),
+        category_id:$("#category_id").val()
+    }
+
+    let datosPeticion=JSON.stringify(data);
+
+    $.ajax({
+        url:"https://g95d7ee77550996-computers.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/computer/computer",
+        data:datosPeticion,
+        type:'POST',
+        contentType:"application/JSON",
+
+        success:function(respuesta){
+            console.log("Insertado");
+            listar();
+        },
+
+        error:function(xhr,status){
+            console.log(status);
+        }
+    });
+
+}
+function listar(){
+    $.ajax({
+        url:"https://g95d7ee77550996-computers.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/computer/computer",
+        type:'GET',
+        dataType:'json',
+
+        success:function(respuesta){
+            console.log(respuesta);
+            listarRespuesta(respuesta.items);
+        },
+
+        error:function(xhr,status){
+            console.log(status);
+        }
+    });
+}
+function listarRespuesta(items){
+    var tabla=`<table border="1" class="table table-dark table-hover">
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Category_id</th>
+                <th colspan="2">Acciones</th>
+              </tr>`;
+    for (var i=0; i<items.length;i++) {
+        tabla+=`<tr>
+                    <td>${items[i].id}</td>
+                    <td>${items[i].brand}</td>
+                    <td>${items[i].model}</td>
+                    <td>${items[i].name}</td>
+                    <td>${items[i].category_id}</td> 
+                    <td><button onclick="editarRegistro(${items[i].id})">Editar</td>
+                    <td><button onclick="borrar(${items[i].id})">Borrar</td>       
+                </tr>
+        `;
+    }
+    tabla+=`</table>`;
+    $("#listado").html(tabla);
+}
+function borrar(numId) {
+    var data={
+        id:numId
+    }
+
+    let datosPeticion=JSON.stringify(data);
+
+    $.ajax({
+        url:"https://g95d7ee77550996-computers.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/computer/computer",
+        data:datosPeticion,
+        type:'DELETE',
+        contentType:"application/JSON",
+
+        success:function(respuesta){
+            console.log("Borrado");
+            listar();
+        },
+
+        error:function(xhr,status){
+            console.log(status);
+        }
+    });
+    
+}
+function editarRegistro(numId) {
+    $("#btnGuardar").show();
+    $("#btnAgregar").hide();
+    $("#btnListar").hide();
+    $("#id").prop('disabled',true);
+    $("#brand").focus();
+    var datos={
+        id:numId
+    }
+    $.ajax({
+        url:"https://g95d7ee77550996-computers.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/computer/computer/"+numId,
+        type:'GET',
+        dataType:'json',
+        success:function(respuesta){
+            var items=respuesta.items;
+            console.log(items);
+            $("#id").val(items[0].id),
+            $("#brand").val(items[0].brand),
+            $("#model").val(items[0].model),
+            $("#category_id").val(items[0].category_id),
+            $("#name").val(items[0].name)
+        },
+        error:function(xhr,status){
+            console.log(status);
+        }
+    });
+ 
+}
+
+function actualizar(){
+    var data={
+        id:$("#id").val(),
+        brand:$("#brand").val(),
+        model:$("#model").val(),
+        category_id:$("#category_id").val(),
+        name:$("#name").val()
+    }
+
+    let datosPeticion=JSON.stringify(data);
+
+    $.ajax({
+        url:"https://g95d7ee77550996-computers.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/computer/computer",
+        data:datosPeticion,
+        type:'PUT',
+        contentType:"application/JSON",
+
+        success:function(respuesta){
+            console.log("Actualizado");
+            listar();
+            $("#btnGuardar").hide();
+            $("#btnAgregar").show();
+            $("#btnListar").show();
+        },
+
+        error:function(xhr,status){
+            console.log(status);
+            alert(status);
+        }
+    });
+}
+
